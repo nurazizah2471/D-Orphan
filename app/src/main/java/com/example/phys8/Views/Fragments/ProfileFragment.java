6311d40aa423f34642f24efa24c35fb8ebb051b9
Nur Azizah <nur.azizah.7241@gmail.com>
2022-11-15 19:18:42 +0800

    profile backend

--- app/src/main/java/com/example/phys8/Views/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/phys8/Views/Fragments/ProfileFragment.java
@@ -1,35 +1,23 @@
 package com.example.phys8.Views.Fragments;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
 
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.phys8.Helpers.SharedPreferenceHelper;
 import com.example.phys8.Models.User;
 import com.example.phys8.R;
 import com.example.phys8.ViewModels.ProfileViewModel;
-
-import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,7 +45,7 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfilFragment.
+     * @return A new instance of fragment profileFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
@@ -78,63 +66,43 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     private ProfileViewModel profileViewModel;
     private SharedPreferenceHelper helper;
-    private ImageView btn_profile_peringkat, btn_profile_history;
-    private TextView txt_stats_ticket, txt_stats_coin, txt_stats_cash, txt_profile_nama;
-    private MediaPlayer mediaPlayer;
-    private ConstraintLayout loadProfile;
-
+    private TextView txt_alamat_fragment_profile, txt_email_fragment_profile,
+            txt_bergabung_sejak_fragment_profile, txt_deskripsi_fragment_profile,
+            txt_name_fragment_profile;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         initial();
 
-        mediaPlayer = MediaPlayer.create(getActivity(), R.raw.clicksound);
-
         profileViewModel.init(helper.getAccessToken());// unsend
-        profileViewModel.getUserWithId2(helper.getUserId());
-        profileViewModel.getResultUserWithId2().observe(getActivity(), showResultUserInfo2);
-
-        btn_profile_peringkat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.start();
-               // Navigation.findNavController(v).navigate(R.id.action_profileFragment_to_peringkatFragment);
-            }
-        });
-
-        btn_profile_history.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mediaPlayer.start();
-             //   Navigation.findNavController(v).navigate(R.id.action_profileFragment_to_historyFragment);
-            }
-        });
+        profileViewModel.getUserWithId(helper.getUserId());
+        profileViewModel.getResultUserWithId().observe(getActivity(), showResultUser);
     }
 
-    private Observer<User.Result> showResultUserInfo2 = new Observer<User.Result>() {
+    private Observer<User.Result> showResultUser = new Observer<User.Result>() {
         @Override
         public void onChanged(User.Result results) {
-            txt_profile_nama.setText(String.valueOf(results.getUsername()));
-            txt_stats_cash.setText(String.valueOf(results.getMyuser().getMoney()));
-            txt_stats_ticket.setText(String.valueOf(results.getMyuser().getTicket()));
-            txt_stats_coin.setText(String.valueOf(results.getMyuser().getScore()));
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    loadProfile.setVisibility(View.GONE); // Hide Progress bar
-                }
-            }, 300);
+            txt_name_fragment_profile.setText(String.valueOf(results.getName()));
+            txt_alamat_fragment_profile.setText(String.valueOf(results.getAlamat()));
+            txt_email_fragment_profile.setText(String.valueOf(results.getEmail()));
+            txt_bergabung_sejak_fragment_profile.setText(String.valueOf(results.getCreated_at()));
+            txt_deskripsi_fragment_profile.setText(String.valueOf(results.getOrphanage().getDeskripsi()));
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    loadProfile.setVisibility(View.GONE); // Hide Progress bar
+//                }
+//            }, 300);
         }
     };
 
@@ -145,13 +113,11 @@ public class ProfileFragment extends Fragment {
     }
 
     private void initial() {
-        btn_profile_peringkat = getActivity().findViewById(R.id.btn_profile_peringkat);
-        btn_profile_history = getActivity().findViewById(R.id.btn_profile_history);
-        txt_stats_cash = getActivity().findViewById(R.id.txt_stats_cash);
-        txt_stats_coin = getActivity().findViewById(R.id.txt_stats_coin);
-        txt_stats_ticket = getActivity().findViewById(R.id.txt_stats_ticket);
-        txt_profile_nama = getActivity().findViewById(R.id.txt_profile_nama);
-        loadProfile = getActivity().findViewById(R.id.loadProfile);
+        txt_name_fragment_profile = getActivity().findViewById(R.id.txt_name_fragment_profile);
+        txt_email_fragment_profile = getActivity().findViewById(R.id.txt_email_fragment_profile);
+        txt_alamat_fragment_profile = getActivity().findViewById(R.id.txt_alamat_fragment_profile);
+        txt_bergabung_sejak_fragment_profile = getActivity().findViewById(R.id.txt_bergabung_sejak_fragment_profile);
+        txt_deskripsi_fragment_profile = getActivity().findViewById(R.id.txt_deskripsi_fragment_profile);
 
         helper = SharedPreferenceHelper.getInstance(requireActivity());
         profileViewModel = new ViewModelProvider(getActivity()).get(ProfileViewModel.class);
